209df76c550b88f27ceab9ceaed7f76ddcb316e2
Avivo1203 <avivoshri6@gmail.com>
2026-05-06 20:43:25 +0300

    Changing major things in mongo connection

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,16 +1,26 @@
-import mongoose from 'mongoose';
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
+  throw new Error("Missing MONGODB_URI");
 }
 
-async function connectToDatabase() {
-  if (mongoose.connection.readyState >= 1) return;
+let cached = global.mongoose;
 
-  return mongoose.connect(MONGODB_URI);
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export default connectToDatabase;
+export async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: process.env.MONGODB_DB || undefined,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
